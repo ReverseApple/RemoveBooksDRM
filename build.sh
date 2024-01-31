1728d078d92c8d611c07d409f7e792e026e10f86
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2024-01-31 09:24:27 -0500

    add system checks to scripts.

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
+if [[ $(nvram boot-args) != *"arm64e_preview_abi"* ]]; then
+	echo "arm64e preview ABI is not enabled."
+	echo "enable it with: sudo nvram boot-args=-arm64e_preview_abi"
+	echo "and then restart your mac."
+	exit 1
+fi
+
 clang -dynamiclib -o injected.dylib injected.m -arch arm64e -framework Foundation
