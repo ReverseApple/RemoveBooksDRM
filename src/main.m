823dde6225f1930bc987213ba03e65540d71a4aa
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2024-08-04 21:54:22 -0400

    Lots of things.

--- src/main.m
+++ src/main.m
@@ -9,28 +9,34 @@
 #include "rdrm.h"
 #import "util.h"
 
-#define MENUBAR_TITLE "ReverseApple"
+#define MENUBAR_TITLE "RemoveBooksDRM"
 
 
-@interface MenuItemHandler : NSObject
+@interface RBDRMDelegate : NSObject
 + (void)build;
 
-+ (MenuItemHandler *)sharedInstance;
++ (RBDRMDelegate *)sharedInstance;
 
-- (void)decryptItemClicked:(id)sender;
+- (void)handleBookOpenNotification:(NSNotification *)notification;
+
+- (void)decryptAllItemsClicked:(id)sender;
 
 - (void)aboutItemClicked:(id)sender;
+
+-(void)decryptAssetWithID:(NSString *)assetID savePath:(NSString *)path;
+
+@property(strong, nonatomic) NSData *bkaContainerPermit;
 @end
 
-@implementation MenuItemHandler
+@implementation RBDRMDelegate
 
 + (void)build {
-    MenuItemHandler *instance = [MenuItemHandler sharedInstance];
+    RBDRMDelegate *instance = [RBDRMDelegate sharedInstance];
 
     NSApplication *app = [NSApplication sharedApplication];
     NSMenu *mainMenu = [app mainMenu];
 
-    if([mainMenu itemWithTitle:@MENUBAR_TITLE] != nil)
+    if ([mainMenu itemWithTitle:@MENUBAR_TITLE] != nil)
         return;
 
     NSMenuItem *rootItem = [[NSMenuItem alloc] initWithTitle:@MENUBAR_TITLE action:nil keyEquivalent:@""];
@@ -39,7 +45,7 @@
     NSMenu *submenu = [[NSMenu alloc] initWithTitle:@MENUBAR_TITLE];
     [rootItem setSubmenu:submenu];
 
-    NSMenuItem *decryptItem = [[NSMenuItem alloc] initWithTitle:@"Decrypt Content" action:@selector(decryptItemClicked:) keyEquivalent:@""];
+    NSMenuItem *decryptItem = [[NSMenuItem alloc] initWithTitle:@"Decrypt All Items" action:@selector(decryptAllItemsClicked:) keyEquivalent:@""];
     NSMenuItem *aboutItem = [[NSMenuItem alloc] initWithTitle:@"About" action:@selector(aboutItemClicked:) keyEquivalent:@""];
 
     [decryptItem setTarget:instance];
@@ -50,19 +56,157 @@
 
 }
 
-+ (MenuItemHandler *)sharedInstance {
-    static MenuItemHandler *sharedInstance = nil;
++ (RBDRMDelegate *)sharedInstance {
+    static RBDRMDelegate *sharedInstance = nil;
     static dispatch_once_t onceToken;
 
     dispatch_once(&onceToken, ^{
         sharedInstance = [[self alloc] init];
+
+        // Subscribe notification to handler...
+        NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+        [notificationCenter addObserver:sharedInstance
+                               selector:@selector(handleBookOpenNotification:)
+                                   name:@"BKBookReaderContentLayoutFinished"
+                                 object:nil];
+        [sharedInstance checkBKAgentDBPermission];
+
     });
 
     return sharedInstance;
 }
 
-- (void)decryptItemClicked:(id)sender {
+- (void)checkBKAgentDBPermission {
+    NSString *BKA_PATH = [@"~/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books" stringByExpandingTildeInPath];
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if (self.bkaContainerPermit != nil) {
+        return;
+    }
+
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setInformativeText:@"RemoveBooksDRM needs permission to read from the local book storage container.\n\n"
+                              "Please click OK when the folder selection prompt appears."];
+    [alert runModal];
+
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    [openPanel setCanChooseFiles:NO];
+    [openPanel setCanChooseDirectories:YES];
+    [openPanel setAllowsMultipleSelection:NO];
+    [openPanel setDirectoryURL:[NSURL fileURLWithPath:BKA_PATH]];
+
+    [openPanel setPrompt:@"OK"];
+    [openPanel setMessage:@"Please click OK. Do not change the directory."];
+
+    [openPanel beginSheetModalForWindow:nil completionHandler:^(NSModalResponse result) {
+        if (result == NSModalResponseOK) {
+            NSURL *selectedFolder = [openPanel URL];
+            NSLog(@"Selected folder: %@", [selectedFolder path]);
+
+            NSError *error;
+            NSArray *directoryContents = [fileManager
+                    contentsOfDirectoryAtPath:[selectedFolder path]
+                                        error:&error];
+
+            if (error == nil) {
+                NSLog(@"Error reading directory: %@", error.localizedDescription);
+            } else {
+                // Store the permit for the folder.
+                NSData *permit = [selectedFolder bookmarkDataWithOptions:NSURLBookmarkCreationWithSecurityScope
+                                          includingResourceValuesForKeys:nil
+                                                           relativeToURL:nil
+                                                                   error:&error];
+
+                if (permit) {
+
+                    NSAlert *successModal = [[NSAlert alloc] init];
+                    [successModal setMessageText:@"Success"];
+                    [successModal setInformativeText:@"Permission acquired successfully."];
+                    [successModal runModal];
+
+                    self.bkaContainerPermit = permit;
+                    NSLog(@"%@", directoryContents);
+                } else {
+                    NSAlert *errModal = [[NSAlert alloc] init];
+                    [errModal setAlertStyle:NSAlertStyleCritical];
+                    [errModal setMessageText:@"Failed to get container permission."];
+                    [errModal setInformativeText:[NSString stringWithFormat:@"Error: %@", error.localizedDescription]];
+                    [errModal runModal];
+                }
+            }
+        } else {
+            NSAlert *errModal = [[NSAlert alloc] init];
+            [errModal setAlertStyle:NSAlertStyleCritical];
+            [errModal setMessageText:@"Failed to get container permission."];
+            [errModal setInformativeText:@"You did not click OK."];
+            [errModal runModal];
+        }
+    }];
+}
+
+- (void)handleBookOpenNotification:(NSNotification *)notification {
+
+    id layoutController = notification.object;
+    id bookEntity = [layoutController safeSwiftValueForKey:@"bookEntity"];
+
+    NSString *bookTitle = [bookEntity safeSwiftValueForKey:@"title"];
+    NSString *bookAuthor = [bookEntity safeSwiftValueForKey:@"author"];
+    NSString *bookAssetID = [bookEntity safeSwiftValueForKey:@"assetID"];
+    NSString *bookAssetPath = [bookEntity safeSwiftValueForKey:@"path"];
+    NSString *bookAssetExtension = [bookAssetPath pathExtension];
+
+    // Present a confirmation alert.
+    NSAlert *confirmation = [[NSAlert alloc] init];
+    NSString *message = [NSString stringWithFormat:@"Do you want to decrypt %@ by %@?", bookTitle, bookAuthor];
+    [confirmation setInformativeText:message];
+    [confirmation addButtonWithTitle:@"Yes"];
+    [confirmation addButtonWithTitle:@"No"];
+    [confirmation setMessageText:@"Decrypt Item?"];
+
+    NSModalResponse response = [confirmation runModal];
+    if (response == NSAlertSecondButtonReturn) {
+        return;
+    }
+
+    // Allow the user to select a place to save the item.
+    NSSavePanel *savePanel = [NSSavePanel savePanel];
+    [savePanel setNameFieldStringValue:[NSString stringWithFormat:@"%@.%@", bookTitle, bookAssetExtension]];
+    [savePanel setPrompt:@"OK"];
+    [savePanel setMessage:@"Select a location to save the decrypted item."];
+
+    [savePanel beginSheetModalForWindow:nil completionHandler: ^(NSModalResponse result) {
+        if (result == NSModalResponseOK) {
+            [self decryptAssetWithID:bookAssetPath savePath:[[savePanel URL] path]];
+        }
+    }];
+
+//    [self decryptAssetWithID:bookAssetID savePath];
+
+}
+
+- (void)decryptAllItemsClicked:(id)sender {
     NSLog(@"Decrypt clicked!");
+
+    NSError *error = nil;
+    BOOL stalePermit;
+    NSURL *containerURL = [NSURL URLByResolvingBookmarkData:self.bkaContainerPermit
+                                                    options:NSURLBookmarkResolutionWithSecurityScope
+                                              relativeToURL:nil
+                                        bookmarkDataIsStale:&stalePermit
+                                                      error:&error];
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    if (containerURL) {
+        if ([containerURL startAccessingSecurityScopedResource]) {
+            NSError *error;
+            NSArray *directoryContents = [fileManager
+                    contentsOfDirectoryAtPath:[containerURL path]
+                                        error:&error];
+            NSLog(@"%@", directoryContents);
+        }
+    }
+
 }
 
 - (void)aboutItemClicked:(id)sender {
@@ -70,7 +214,7 @@
     NSAlert *about = [[NSAlert alloc] init];
 
     [about setMessageText:@"RemoveBooksDRM"];
-    [about setInformativeText:@"Version 2.0.0\n"
+    [about setInformativeText:@"Version 2.0.0\n\n"
                               "Bypass: @AngeloD2022\n"
                               "Implementation: @AngeloD2022, @JJTech0130\n\n"
                               "ReverseApple, 2024\n"
@@ -78,6 +222,7 @@
 
     [about runModal];
 }
+
 @end
 
 
@@ -93,8 +238,8 @@ static void injected(int argc, const char **argv) {
                          queue:[NSOperationQueue mainQueue]
                     usingBlock:^(NSNotification *note) {
                         [Utilities debounced:^{
-                            [MenuItemHandler build];
-                        } withDelay:1];
+                            [RBDRMDelegate build];
+                        }          withDelay:1];
                     }];
 
 }
--- src/rdrm.m
+++ src/rdrm.m
@@ -8,7 +8,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#import "obfuscated.h"
+#import "external.h"
 #include "rdrm.h"
 
 
